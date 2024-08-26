54183407d8037922a9f09ce05220339946ccf0aa
Petehns <pedronunesufpe@gmail.com>
2024-08-26 10:46:28 -0300

    Initial Commit

---
+++ challengeTribos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  challengeTribos
+//
+//  Created by Pedro Henrique Nunes da Silveira Bezerra on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("opa")
+            Text("manooo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ challengeTribos/challengeTribosApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengeTribosApp.swift
+//  challengeTribos
+//
+//  Created by Pedro Henrique Nunes da Silveira Bezerra on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct challengeTribosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
