c71b8486eebf4f8efa495550bbc98c3c85c3e508
Pedro Nunes Silveira <89811554+Petehns@users.noreply.github.com>
2024-08-26 10:59:19 -0300

    Update ContentView.swift

--- challengeTribos/ContentView.swift
+++ challengeTribos/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Text("manooo")
             Text("manda a braba")
             Text("manda a braba")
-            Text("marmelada")
+            Text("marmelkada")
         }
         .padding()
     }
