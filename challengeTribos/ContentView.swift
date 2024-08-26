31d93a2b7a3c2158ca30e570b6660b80bdb0f597
Petehns <pedronunesufpe@gmail.com>
2024-08-26 10:50:52 -0300

    sla

--- challengeTribos/ContentView.swift
+++ challengeTribos/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("opa")
             Text("manooo")
             Text("manda a braba")
+            Text("manda a braba")
         }
         .padding()
     }
