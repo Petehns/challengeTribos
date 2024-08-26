9b18aec2c34bd05942bdd034131d72caf7161073
Petehns <pedronunesufpe@gmail.com>
2024-08-26 10:47:24 -0300

    Adicionando texto

--- challengeTribos/ContentView.swift
+++ challengeTribos/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("opa")
             Text("manooo")
+            Text("manda a braba")
         }
         .padding()
     }
